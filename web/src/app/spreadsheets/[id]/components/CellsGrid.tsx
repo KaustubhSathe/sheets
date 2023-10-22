f2274328f4329a6618cb75735791225277096741
Kaustubh Sathe <f2015466@pilani.bits-pilani.ac.in>
2023-10-22 23:34:25 +0530

    Web (#5)
    * chore: add more code
    
    * chore: add more code
    
    ---------
    
    Co-authored-by: Kaustubh <ksathe@suki.ai>

--- web/src/app/spreadsheets/[id]/components/CellsGrid.tsx
+++ web/src/app/spreadsheets/[id]/components/CellsGrid.tsx
@@ -4,16 +4,16 @@ export default function CellsGrid() {
     const rowsNumbers: Array<React.ReactNode> = [];
     for (let i = 0; i < 1000; i++) {
         rowsNumbers.push((
-            <div className="h-[30px] w-[46px] flex justify-center align-middle">
+            <div className="h-[30px] w-full flex justify-center align-middle border-b-[1px] border-r-[1px] border-solid border-[#E1E1E1] font-sans">
                 {i + 1}
             </div>
         ));
     }
 
     const colNumbers: Array<React.ReactNode> = [];
-    for (let i = 0; i < 26; i++) {
+    for (let i = 0; i < 50; i++) {
         colNumbers.push((
-            <div className="h-[30px] w-[46px] flex justify-center align-middle text-center">
+            <div className="min-w-[66px] h-full inline-block text-center border-b-[1px] border-t-[1px] border-r-[1px] border-solid border-[#E1E1E1]">
                 {String.fromCharCode(65 + i)}
             </div>
         ));
@@ -22,12 +22,15 @@ export default function CellsGrid() {
     const cells: Array<React.ReactNode> = [];
     for (let i = 0; i < 1000; i++) {
         const x: Array<React.ReactNode> = [];
-        for (let j = 0; j < 26; j++) {
-            let cell = <div className={`h-[30px] w-[46px] border border-[1px] border-solid border-amber-600 contenteditable='true' spellcheck='false' rid=${i} cid=${j}`}></div>;
+        for (let j = 0; j < 36; j++) {
+            let cell = <div className={`pl-[4px] outline-none break-words break-all h-full min-w-[66px] inline-block border-b-[1px] border-r-[1px] border-solid border-[#E1E1E1] rid=${i} cid=${j}`}
+                contentEditable={true}
+                spellCheck={false}
+            ></div>;
             x.push(cell);
         }
         cells.push((
-            <div className="flex">
+            <div className="h-[30px] overflow-x-hidden overflow-y-hidden">
                 {x}
             </div>
         ));
@@ -36,15 +39,15 @@ export default function CellsGrid() {
     return (
         <div className="bg-[#FFFFFF] h-[calc(100vh-60px-40px-35px-37px)] relative overflow-scroll p-0 m-0">
             <div className="fixed bg-[#747d8c] h-[30px] w-[46px] z-10"></div>
-            <div className="sticky mt-[30px] left-0 bg-slate-200 w-[46px]">
+            <div className="sticky mt-[30px] left-0 bg-inherit w-[46px] overflow-x-hidden overflow-y-hidden">
                 {
                     rowsNumbers
                 }
             </div>
-            <div className="absolute top-0 left-[46px] w-[100%] h-[30px] bg-orange-500 m-0">
-                <div className="flex sticky top-0 left-[46px] bg-green-700">
+            <div className="absolute top-0 left-[46px] w-[100%] h-[30px] bg-inherit m-0">
+                <div className="h-[30px] flex top-0 left-[46px] bg-inherit">
                     {
-                        colNumbers
+                        colNumbers.map(x => x)
                     }
                 </div>
                 {
--- web/src/app/spreadsheets/[id]/page.tsx
+++ web/src/app/spreadsheets/[id]/page.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import CellsGrid from "./components/CellsGrid";
 import FormulaBar from "./components/FormulaBar";
 import MenuBar from "./components/MenuBar";
 import SheetsBar from "./components/SheetsBar";
 import ToolsBar from "./components/ToolsBar";
+import { Provider } from 'react-redux'
+import store from '../../lib/redux/store'
+
 
 export default function Spreadsheet() {
     return (
-        <>
+        <Provider store={store}>
             <MenuBar />
             <ToolsBar />
             <FormulaBar />
             <CellsGrid />
             <SheetsBar />
-        </>
+        </Provider>
     )
 }
