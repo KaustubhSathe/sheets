aa20e713b38ff7ed1814109d942e0e4d81c0b5dc
Kaustubh <ksathe@suki.ai>
2023-10-23 18:29:45 +0530

    chore: remove max

--- web/src/app/spreadsheets/[id]/page.tsx
+++ web/src/app/spreadsheets/[id]/page.tsx
@@ -8,8 +8,6 @@ import ToolsBar from "./components/ToolsBar";
 import { Provider } from 'react-redux'
 import store from '../../lib/redux/store'
 
-export const maxDuration = 100; // This function can run for a maximum of 100 seconds
-
 
 export default function Spreadsheet() {
     return (
