08b02b4d5ef39ac9d0e1437c19bd99b40869188e
Kaustubh Sathe <f2015466@pilani.bits-pilani.ac.in>
2023-10-23 17:21:33 +0530

    chore: more code

--- web/src/app/spreadsheets/[id]/components/CellsGrid.tsx
+++ web/src/app/spreadsheets/[id]/components/CellsGrid.tsx
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux'
 
 export default function CellsGrid() {
     const dispatch = useDispatch()
-
+    
     const rowsNumbers: Array<React.ReactNode> = [];
     for (let i = 0; i < 1000; i++) {
         rowsNumbers.push((
@@ -30,21 +30,27 @@ export default function CellsGrid() {
     }
 
     const cells: Array<React.ReactNode> = [];
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 100; i++) {
         const x: Array<React.ReactNode> = [];
         for (let j = 0; j < 50; j++) {
-            x.push(<div className="focus:border-[#1a73e8] focus:border-[3px] overflow-x-hidden overflow-y-hidden pl-[4px] outline-none break-words break-all h-full min-w-[66px] border-b-[1px] border-r-[1px] border-solid border-[#E1E1E1]"
-                contentEditable={true}
-                spellCheck={false}
-                id={String.fromCharCode(65 + j) + (i + 1).toString()}
-                onClick={(e) => {
-                    dispatch(setValue(e.currentTarget.id))
-                }}
-                onInput={(e) => {
-                    dispatch(setValueFormulaBar(e.currentTarget.outerText))
-                }}
-                key={String.fromCharCode(65 + j) + (i + 1).toString()}
-            ></div>);
+            x.push(
+                <div className="relative m-0 p-0 h-full min-w-[66px]">
+                    <div className="peer focus:border-[#1a73e8] focus:border-[3px] overflow-x-hidden overflow-y-hidden pl-[4px] outline-none break-words break-all h-full w-full border-b-[1px] border-r-[1px] border-solid border-[#E1E1E1]"
+                        contentEditable={true}
+                        spellCheck={false}
+                        id={String.fromCharCode(65 + j) + (i + 1).toString()}
+                        onClick={(e) => {
+                            dispatch(setValue(e.currentTarget.id))
+                        }}
+                        onInput={(e) => {
+                            dispatch(setValueFormulaBar(e.currentTarget.outerText))
+                        }}
+                        key={String.fromCharCode(65 + j) + (i + 1).toString()}
+                    ></div>
+                    <div className="absolute -z-10 bottom-[-3px] right-[-3px] w-[10px] h-[10px] rounded-full peer-focus:bg-[#1a73e8] peer-focus:hover:cursor-crosshair peer-focus:z-10">
+                    </div>
+                </div>
+            );
         }
         cells.push((
             <div className="h-[30px] flex bg-inherit" key={(i + 1).toString()}>
@@ -61,7 +67,7 @@ export default function CellsGrid() {
                     rowsNumbers
                 }
             </div>
-            <div className="absolute top-0 left-[46px] h-[calc(100vh-60px-40px-35px-37px)] w-full bg-inherit m-0">
+            <div className="hover:cursor-cell absolute top-0 left-[46px] h-[calc(100vh-60px-40px-35px-37px)] w-full bg-inherit m-0">
                 <div className="h-[30px] flex bg-inherit">
                     {
                         colNumbers
