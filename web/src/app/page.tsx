2a686006e695ca59f8b19d8148343dfb70e5f12b
Kaustubh Sathe <f2015466@pilani.bits-pilani.ac.in>
2023-10-26 01:54:51 +0530

    chore: add login page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,55 @@
+import { AiFillGithub, AiFillAndroid, AiFillApple, AiFillChrome, AiOutlineDesktop } from 'react-icons/ai'
+import Sheet from '../../public/sheets.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black text-white flex align-middle justify-center text-center">
-      Landing Page
+    <div className="w-full h-screen flex bg-slate-200">
+      <div className="h-full w-[70%] flex flex-col align-middle justify-center gap-5">
+        <div className='flex justify-center'>
+          <span className="font-sans text-4xl font-semibold">Make data-driven decisions, in Work Sheets</span>
+        </div>
+        <div className='flex justify-center'>
+          <span className="font-sans text-xl font-normal">Create and collaborate on online spreadsheets in real-time and from any device.</span>
+        </div>
+        <div className='flex justify-center'>
+          <span className="font-sans text-3xl font-semibold">Available On:</span>
+        </div>
+        <div className="flex align-middle justify-center gap-3">
+          <AiFillChrome className="w-[50px] h-[50px]" />
+          <AiFillAndroid className="w-[50px] h-[50px]" />
+          <AiFillApple className="w-[50px] h-[50px]" />
+          <AiOutlineDesktop className="w-[50px] h-[50px]" />
+        </div>
+        <div className='flex justify-center'>
+          <span className="font-sans text-2xl font-normal">Check the code here: <a href="https://github.com/KaustubhSathe/spreadsheet" target='_blank' className='text-blue-500 hover:text-blue-700'>Github</a></span>
+        </div>
+        <div className='flex justify-center'>
+          <span className="font-sans text-2xl font-normal">Want to know how I built this. Click here: <a href="https://www.udemy.com/user/kaustubh-sathe-5/" target='_blank' className='text-blue-500 hover:text-blue-700'>Udemy</a></span>
+        </div>
+      </div>
+
+      {/* Login form */}
+      <div className="h-full w-[30%] text-center p-[8px]">
+        <div className="border-[2px] h-full border-[#34a853] rounded-xl flex flex-col align-middle justify-center">
+          <div className="flex align-middle justify-center gap-3 mb-[15px]">
+            <Image title='Sheets' width={27} height={27} src={Sheet} alt="sheet-icon" className="mt-auto mb-auto" />
+            <span className="mt-auto mb-auto font-sans font-bold text-xl">Work Sheets</span>
+          </div>
+          <div className='mb-[8px]'>
+            <span className="font-bold">Welcome!!</span> &#128075;
+          </div>
+          <div>
+            <span className="font-semibold font-sans">
+              Login to unlock access to Work Sheets!!!
+            </span>
+          </div>
+          <div className="hover:bg-[#34a853] hover:cursor-pointer hover:scale-[1.02] hover:shadow-md hover:shadow-black h-[40px] m-[8px] mt-[16px] bg-in rounded-xl flex align-middle justify-center gap-3 border-[2px] border-black">
+            <AiFillGithub className="w-[25px] h-[25px] mt-auto mb-auto" />
+            <span className='mt-auto mb-auto font-bold'>Continue with Github</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
