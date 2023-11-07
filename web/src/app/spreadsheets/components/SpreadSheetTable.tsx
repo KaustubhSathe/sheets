b7878e36c88fb04bde3c53a9002b9c9c0de9fac2
Kaustubh <ksathe@suki.ai>
2023-11-08 04:25:22 +0530

    chore: make responsive

--- web/src/app/spreadsheets/components/SpreadSheetTable.tsx
+++ web/src/app/spreadsheets/components/SpreadSheetTable.tsx
@@ -19,10 +19,10 @@ export default function SpreadSheetTable({ spreadsheets }: { spreadsheets: Sprea
     return (
         <div className="w-full mt-4">
             <div className="font-bold font-roboto text-[#212b3e] flex w-full justify-between m-0">
-                <div className="text-left ml-4">
+                <div className="text-left ml-4 sm:ml-6">
                     SpreadSheet Title
                 </div>
-                <div className="text-center invisible sm:visible">
+                <div className="text-center hidden sm:block mr-[23%]">
                     Last Opened
                 </div>
             </div>
@@ -31,7 +31,8 @@ export default function SpreadSheetTable({ spreadsheets }: { spreadsheets: Sprea
                     return (new Date(b.LastOpened).getTime() - new Date(a.LastOpened).getTime());
                 }).map(ss => (
                     <div key={ss.SK} className="h-[50px] mt-2 mb-2 hover:bg-[#e3f7ea] hover:rounded-full hover:cursor-pointer flex align-middle justify-between">
-                        <div className="mt-auto mb-auto flex justify-center align-middle">
+                        {/* For mobile sizes */}
+                        <div className="mt-auto mb-auto flex justify-center align-middle sm:hidden">
                             <div className="ml-3 flex align-middle justify-center">
                                 <BsFillFileEarmarkSpreadsheetFill className="w-[20px] h-[20px] mt-auto mb-auto" />
                             </div>
@@ -44,8 +45,26 @@ export default function SpreadSheetTable({ spreadsheets }: { spreadsheets: Sprea
                                 </div>
                             </div>
                         </div>
-                        <div className="mr-2 text-center flex align-middle justify-center hover:bg-slate-200 h-[40px] w-[40px] hover:rounded-full mt-auto mb-auto">
+                        <div className="sm:hidden mr-2 text-center flex align-middle justify-center hover:bg-slate-200 min-h-[40px] min-w-[40px] hover:rounded-full mt-auto mb-auto">
                             <SlOptionsVertical className="w-[20px] h-[20px] mt-auto mb-auto" />
+                        </div>
+
+                        {/* For greater than sm sizes */}
+                        <div className="justify-center align-middle sm:block hidden mt-auto mb-auto">
+                            <div className="ml-4 align-middle justify-center inline-block">
+                                <BsFillFileEarmarkSpreadsheetFill className="w-[20px] h-[20px] mt-auto mb-auto" />
+                            </div>
+                            <div className="ml-4 align-middle justify-center inline-block">
+                                <span className="mt-auto mb-auto font-roboto font-normal text-sm">{ss.SpreadSheetTitle}</span>
+                            </div>
+                        </div>
+                        <div className="sm:flex sm:align-middle sm:justify-between sm:w-[50%] hidden">
+                            <div className="text-center mt-auto w-full h-full mb-auto align-middle flex justify-center">
+                                <span className="mt-auto mb-auto text-[#786F6D] text-xs">{new Date(ss.LastOpened).toLocaleString()}</span>
+                            </div>
+                            <div className="mr-2 text-center hover:bg-slate-200 min-h-[40px] min-w-[40px] hover:rounded-full mt-auto mb-auto align-middle justify-center flex">
+                                <SlOptionsVertical className="w-[20px] h-[20px] m-auto" />
+                            </div>
                         </div>
                     </div>
                 ))
